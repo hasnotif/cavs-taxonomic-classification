1f6edbb4d4f01033bcc175077a1ea93311f12148
Irsyaad <hasifirsyaad@gmail.com>
2022-03-25 16:14:46 +0800

    Added R tree generator script

---
+++ generate_radial_tree.R
@@ -0,0 +1,49 @@
+library(rio)
+library(here)
+library(janitor)
+library(lubridate)
+library(epikit)
+library(skimr)
+library(ggplot2)
+library(ggtree)
+library(ggtreeExtra)
+library(treeio)
+library(plotly)
+
+Sys.setenv(QT_QPA_PLATFORM = "offscreen")
+args <- commandArgs(trailingOnly = TRUE) # requires ETE3-generated newick tree + Kraken2/Bracken report
+
+if (length(args) == 0) {
+  stop("No input tree file found. Please specify an input tree file.", call. = FALSE)
+} else if (length(args) == 1) {
+  stop("No input Kraken2/Bracken report file found. Please specify an input Kraken2/Bracken report file.", call. = FALSE)
+}
+
+# Importing Kraken2/Bracken sample report output format and ETE3-generated Newick taxonomy tree
+tree <- treeio::read.nhx(args[1])
+report <- import(args[2])
+colnames(report) <- c("percentage_cover", "num_cover", "num_direct", "rank_code", "ncbi_taxid", "sci_name")
+report <- select(report, ncbi_taxid, sci_name, rank_code, num_direct)
+
+tibble <- as_tibble(tree)
+tibble_phyla <- filter(tibble, tibble$rank == "phylum")
+colnames(tibble_phyla)[7] <- "Phyla"
+
+p <- ggtree(tree, layout = "circular", branch.length = "none") +
+     geom_tippoint(color = "black", size = 1) +
+     geom_hilight(data = tibble_phyla, mapping = aes(node = node, fill = Phyla), alpha = 0.5) +
+     geom_fruit(
+      data = report,
+      mapping = aes(x = num_direct, y = ncbi_taxid),
+      geom = geom_bar,
+      orientation = "y",
+      stat = "identity",
+      fill = "black") +
+     ggtitle("Taxonomy tree of Kraken2-classified groups (after Bracken estimation)") +
+     theme(
+      legend.position = "right",
+      legend.title = element_text(face = "bold", size = 10),
+      legend.text = element_text(size = 8),
+      plot.title = element_text(face = "bold", size = 12))
+
+ggsave("radial_tree.pdf")
